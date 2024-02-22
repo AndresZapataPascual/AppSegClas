dbd38f3d9e84d5640e7bc71030f56caffd7816dd
AndresZapataPascual <andres.zapata019@pascualbravo.edu.co>
2024-02-21 23:18:12 -0500

    intent agregados
    se crearon los intent de crear alarma y abrir camara

--- app/src/main/java/com/andreszapata/appsegclas/MainActivity.java
+++ app/src/main/java/com/andreszapata/appsegclas/MainActivity.java
@@ -1,9 +1,13 @@
 package com.andreszapata.appsegclas;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.AlarmClock;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +17,7 @@ import android.content.Intent;
 public class MainActivity extends AppCompatActivity {
 
     TextView nombreUsuario;
-    Button btn1,btn2, Alarma;
+    Button btn1,btn2, Send, Alarma,Camara;
     String Tag = "Prueba";
 
     @Override
@@ -24,14 +28,15 @@ public class MainActivity extends AppCompatActivity {
         nombreUsuario = (TextView) findViewById(R.id.usuario);
         btn1 = findViewById(R.id.btn1);
         btn2 = findViewById(R.id.btn2);
+        Send =findViewById(R.id.btnSend);
         Alarma = findViewById(R.id.Alarma);
+        Camara = findViewById(R.id.Camara);
 
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 nombreUsuario.setText("Presioné Boton Uno");
-                createAlarm("Despertar",  8,  30);
             }
         });
 
@@ -41,12 +46,59 @@ public class MainActivity extends AppCompatActivity {
                 nombreUsuario.setText("Presioné Boton Dos");
             }
         });
+
+        Send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pasarInformacion();
+            }
+        });
+
+        Alarma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createAlarm("Despertar",  8,  30);
+            }
+        });
+
+        Camara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
     }
 
+    private void pasarInformacion() {
+        String textActual = nombreUsuario.getText().toString();
+        String nombre = "Andres Zapata";
+        Intent PasarInfo = new Intent(this,PantallaTwo.class);
+
+        PasarInfo.putExtra("name",nombre);
+        startActivity(PasarInfo);
+    }
     @Override
     protected void onStart() {
         super.onStart();
         Log.i(Tag,"Estoy en OnStart");
+
+        nombreUsuario = (TextView) findViewById(R.id.usuario);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nombreUsuario.setText("Inicia Seccion nuevamente");
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nombreUsuario.setText("Inicia Sección");
+            }
+        });
     }
 
     @Override
@@ -88,4 +140,16 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
     }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // Display error state to the user.
+        }
+    }
+
 }
---
+++ app/src/main/java/com/andreszapata/appsegclas/PantallaTwo.java
@@ -0,0 +1,34 @@
+package com.andreszapata.appsegclas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class PantallaTwo extends AppCompatActivity{
+
+    TextView usuarioDos, textoAnterior;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_two2);
+        usuarioDos = findViewById(R.id.usuarioDos);
+        textoAnterior = findViewById(R.id.textoAnterior);
+
+        String name = getIntent().getStringExtra("name");
+        usuarioDos.setText(name);
+
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        setContentView(R.layout.activity_pantalla_two2);
+        usuarioDos = findViewById(R.id.usuarioDos);
+        textoAnterior = findViewById(R.id.textoAnterior);
+
+        String name = getIntent().getStringExtra("name");
+        usuarioDos.setText(name);
+    }
+}
