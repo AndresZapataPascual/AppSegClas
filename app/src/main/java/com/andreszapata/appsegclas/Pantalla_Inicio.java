9255c281f1050ec7a0ab6c2e405282467f469e26
AndresZapataPascual <andres.zapata019@pascualbravo.edu.co>
2024-02-22 09:26:22 -0500

    Animación
    se creo la animación y una pantalla nueva para ejecutarla

--- app/src/main/java/com/andreszapata/appsegclas/Pantalla_Inicio.java
+++ app/src/main/java/com/andreszapata/appsegclas/Pantalla_Inicio.java
@@ -2,7 +2,9 @@ package com.andreszapata.appsegclas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Pantalla_Inicio extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class Pantalla_Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_inicio);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Pantalla_Inicio.this,MainActivity.class));
+                finish();
+            }
+        }, 9000);
     }
 }
